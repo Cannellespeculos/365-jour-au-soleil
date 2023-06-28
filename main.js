6fed07615d2c2a499da3dfaf60a9bd4489c9e32b
Cannellespeculos <cjeanpierre@edenschool.fr>
2023-06-28 11:47:35 +0200

    finished the header and start main

--- main.js
+++ main.js
@@ -1,24 +1,91 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+const previsions = document.getElementById("prévisions");
+
+const meteo = [
+  {
+    title_day: "lun",
+    title_date: "20",
+    icon: "chance-storm.png",
+    temp_max: "14",
+    temp_min: "4"
+  },
+  {
+    title_day: "mar",
+    title_date: "21",
+    icon: "thunder-storm.png",
+    temp_max: "10",
+    temp_min: "4"
+  },
+  {
+    title_day: "mer",
+    title_date: "22",
+    icon: "sunny.png",
+    temp_max: "19",
+    temp_min: "4"
+  },
+  {
+    title_day: "jeu",
+    title_date: "23",
+    icon: "wind.png",
+    temp_max: "16",
+    temp_min: "4"
+  },
+  {
+    title_day: "ven",
+    title_date: "24",
+    icon: "chance-storm.png",
+    temp_max: "14",
+    temp_min: "4"
+  },
+  {
+    title_day: "sam",
+    title_date: "25",
+    icon: "chance-storm.png",
+    temp_max: "14",
+    temp_min: "4"
+  },
+  {
+    title_day: "dim",
+    title_date: "26",
+    icon: "snow.png",
+    temp_max: "14",
+    temp_min: "4"
+  },
+  {
+    title_day: "lun",
+    title_date: "27",
+    icon: "smoke.png",
+    temp_max: "14",
+    temp_min: "4"
+  },
+  {
+    title_day: "mar",
+    title_date: "28",
+    icon: "sleet.png",
+    temp_max: "14",
+    temp_min: "4"
+  },
+  {
+    title_day: "mer",
+    title_date: "29",
+    icon: "chance-storm.png",
+    temp_max: "14",
+    temp_min: "4"
+  }
+]
+
+for (let i = 0; i < meteo.length; i++) {
+  previsions.innerHTML += `
+  <li>
+    <article>
+      <h3>${meteo[i].title_day}</h3>
+      <h4>${meteo[i].title_date}</h4>
+      <img src="${meteo[i].icon}"  alt="meteos icon ${i}">
+      <p>${meteo[i].temp_max} | <span>${meteo[i].temp_min}</span></p>
+    </article>
+  </li>
+  `
+
+}
